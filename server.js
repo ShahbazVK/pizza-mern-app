8861ba5f0e21b9a4ee9286f644bbe23ccd13dfb0
Shahbaz Ali <shahbazalivk@gmail.com>
2022-10-05 21:45:25 +0500

    Pizza path with slash sign

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(cors())
 app.use("/api/pizzas", require('./routes/pizzaRoute'))
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, 'client/build')))
+    app.use(express.static(path.join(__dirname, '/client/build')))
     app.get("*", (req, res) => {
         res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
     })
